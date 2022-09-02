fca26d9a94a4de67ceb4765f9b7c2ae23b7a8ff8
lucadentella <luca@dentella.it>
2022-09-02 08:47:07 +0200

    Variable address (and corresponding CV) added

--- Arduino/MyFirstDccDecoder/MyFirstDccDecoder.ino
+++ Arduino/MyFirstDccDecoder/MyFirstDccDecoder.ino
@@ -1,14 +1,17 @@
 #include <NmraDcc.h>
 
 #define DCC_PIN 2
+#define ACK_PIN 3
 #define RED_LED 4
 #define GREEN_LED 5
+
+#define DECODER_ADDR 4
 
 NmraDcc Dcc;
 
 void notifyDccAccTurnoutOutput(uint16_t Addr, uint8_t Direction, uint8_t OutputPower) {
 
-  if(Addr == 4) {
+  if(Addr == DECODER_ADDR) {
     if(Direction == 0) {
       digitalWrite(RED_LED, HIGH);
       digitalWrite(GREEN_LED, LOW);
@@ -24,13 +27,23 @@ void notifyDccAccTurnoutOutput(uint16_t Addr, uint8_t Direction, uint8_t OutputP
   Serial.println("--------------------");
 }
 
+void notifyCVAck(void) {
+
+  digitalWrite(ACK_PIN, HIGH);
+  delay(10);  
+  digitalWrite(ACK_PIN, LOW);
+}
+
 void setup() {
 
   Serial.begin(115200);
+  pinMode(ACK_PIN, OUTPUT);
   pinMode(RED_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT); 
   Dcc.pin(digitalPinToInterrupt(DCC_PIN), DCC_PIN, false);
   Dcc.init(MAN_ID_DIY, 1, FLAGS_DCC_ACCESSORY_DECODER | FLAGS_OUTPUT_ADDRESS_MODE, 0);
+  Dcc.setCV(CV_ACCESSORY_DECODER_ADDRESS_LSB, DECODER_ADDR & 0xFF);
+  Dcc.setCV(CV_ACCESSORY_DECODER_ADDRESS_MSB, DECODER_ADDR >> 8);
 }
 
 void loop() {
